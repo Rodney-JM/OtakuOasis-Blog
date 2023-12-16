5592ff02edc8e9f7949bed9dfb7b460fd7d389c0
J.Rodney <148403040+Rodney-JM@users.noreply.github.com>
2023-12-16 17:33:03 -0300

    Delete script directory

--- script/menu.js
+++
@@ -1,7 +0,0 @@
-const navbar = document.querySelector(".header");
-const menuButton = document.querySelector(".menu-button");
-
-
-menuButton.addEventListener('click', ()=> {
-    navbar.classList.toggle("show-menu");
-})
